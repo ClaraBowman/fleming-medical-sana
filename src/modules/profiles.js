438731e87baa35012cb3117bc2ba0a2c76e34120
David Matthew <davidmatthew.ie@gmail.com>
2023-02-17 11:39:32 +0000

    Formatting fixes

--- src/modules/profiles.js
+++ src/modules/profiles.js
@@ -5,7 +5,7 @@ export default function profiles() {
   if (document.getElementById('profiles')) {
     let profiles = document.getElementsByClassName('profile-card');
 
-    for(let i = 0; i < profiles.length; i ++) {
+    for (let i = 0; i < profiles.length; i ++) {
       profiles[i].addEventListener('click', function() {
         this.classList.toggle('expanded');
       });
