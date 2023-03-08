382d2a150ee61a6682b3b6a1469c048235b59911
David Matthew <davidmatthew.ie@gmail.com>
2023-03-08 11:24:40 +0000

    Removed obsolete alternate links functionality

--- src/modules/header.js
+++ src/modules/header.js
@@ -9,22 +9,20 @@ function flagLinks() {
   let countryLink = document.createElement("div");
   countryLink.classList.add("top-dropdown", "language");
 
-  // Only populate the path component of the url if there exists a rel alternate link to the UK site.
-  let validPath = (document.querySelector('link[hreflang="en-GB"]')) ? window.location.pathname : '';
-
   // Conditionally populate the HTML.
   countryLink.innerHTML = `
     <div class="opener">
       <img class="flag" src="/content/files/images/languages/flag-${isIrishSite() ? 'ie' : 'uk'}.svg" alt="Fleming Medical ${isIrishSite() ? 'IE' : 'UK'}" >
     </div>
     <div class="summary">
-      <a class="hyp selected" href="${validPath}">
+      <a class="hyp selected" href="/">
         <img src="/content/files/images/languages/flag-${isIrishSite() ? 'ie' : 'uk'}.svg" alt="Fleming Medical ${isIrishSite() ? 'IE' : 'UK'}" class="flag">Fleming Medical ${isIrishSite() ? 'IE' : 'UK'}
       </a>
-      <a href="https://www.flemingmedical.${isIrishSite() ? 'co.uk' : 'ie'}${validPath}" class="hyp">
+      <a href="https://www.flemingmedical.${isIrishSite() ? 'co.uk' : 'ie'}" class="hyp">
         <img src="/content/files/images/languages/flag-${isIrishSite() ? 'uk' : 'ie'}.svg" alt="Fleming Medical ${isIrishSite() ? 'UK' : 'IE'}" class="flag">Fleming Medical ${isIrishSite() ? 'UK' : 'IE'}
       </a>
-    </div>`;
+    </div>
+  `;
   
   // Prepend it to the relevant containing element.
   let topAction = document.querySelector(".top-header .top-action");
